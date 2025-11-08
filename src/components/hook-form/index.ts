85c580d043a63a35969a737aeaaa143c1232b7d0
Anzai <55052716+owari-hash@users.noreply.github.com>
2025-11-08 14:58:44 +0800

    z

--- src/components/hook-form/index.ts
+++ src/components/hook-form/index.ts
@@ -3,7 +3,7 @@ export * from './rhf-checkbox';
 
 export { default } from './form-provider';
 export { default as RHFCode } from './rhf-code';
-export { default as RHFSelect } from './rhf-select';
+export { RHFSelect } from './rhf-select';
 export { default as RHFSlider } from './rhf-slider';
 export { default as RHFSwitch } from './rhf-switch';
 export { default as RHFTextField } from './rhf-text-field';
